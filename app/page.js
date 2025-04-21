ceef3e63470704643cd3e702d33cc66d31076022
rashmi-punia <rashmipunia909@gmail.com>
2025-04-22 01:58:05 +0530

    Update files

--- app/page.js
+++ app/page.js
@@ -1,9 +1,12 @@
+import Form from "@/components/Form";
+import ProgramCard from "@/components/ProgramCard";
 import Image from "next/image";
 import React from "react";
 
 export default function Home() {
   return (
     <section id="#home" className="relative -mt-24 pt-48 pb-10  ">
+    {/* WAVES nd svgs */}
       <div className="w-full rotate-180 top-0 absolute">
         <img
           src="/svg/wave.svg"
@@ -22,13 +25,13 @@ export default function Home() {
           className="w-full h-full" // Increased height
         />
       </div>
-      <div className="-rotate-12 top-[350px] left-[25vw]  absolute">
+      <div className="-rotate-12 top-[330px] left-[25vw]  absolute">
         <img
           src="/svg/Bus.svg"
-          width={100}
+          width={120}
           height={200}
           alt="wave-svg"
-          className="w- -full opacity-65" // Increased height
+          className="w- -full opacity-70" // Increased height
         />
       </div>
       <div className="rotate-12 top-60 right-20 absolute">
@@ -41,25 +44,12 @@ export default function Home() {
         />
       </div>
 
-      {/* <svg
-        className="absolute bottom-0 w-[120rem] right-10 -left-5"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1440 320"
-      >
-        <path
-          // fill="#ff5500"
-          fill="#FF6F61"
-          fill-opacity="1"
-          d="M0,192L30,165.3C60,139,120,85,180,80C240,75,300,117,360,160C420,203,480,245,540,261.3C600,277,660,267,720,240C780,213,840,171,900,170.7C960,171,1020,213,1080,208C1140,203,1200,149,1260,122.7C1320,96,1380,96,1410,96L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
-        ></path>
-      </svg> */}
-
       {/* Hero */}
 
       <div className="max-w-[85vw]  mx-28 relative  flex flex-col md:flex-row justify-between gap-16">
         {/* Text Content */}
-        <div className="max-w-[500px] text-p_white text-center md:text-left">
-          <h1 className="heading text-4xl">
+        <div className="max-w-[550px] text-p_white text-center md:text-left">
+          <h1 className="heading text-5xl">
             Build Stunning Websites with Ease
           </h1>
           <p className="mt-3 text-lg sub-heading">
@@ -85,24 +75,286 @@ export default function Home() {
       </div>
 
       {/* ABOUT US */}
-      <div className="flex flex-row-reverse justify-between mx-40 my-24">
-        <div className="max-w-3xl">
-          <h1 className="heading text-3xl">About us</h1>
+      <div className="flex flex-row-reverse justify-center gap-32 mx-40 my-24 relative">
+        <Image
+          src={"/svg/PaintBrush.svg"}
+          width={"70"}
+          height={"100"}
+          alt="paintbrush"
+          className="absolute right-0 rotate-12 bottom-12 opacity-65"
+        />
+        <Image
+          src={"/svg/cookies.svg"}
+          width={"80"}
+          height={"100"}
+          alt="paintbrush"
+          className="absolute  -left-16 top-10 opacity-70"
+        />
+
+        <div className="w-[800px]">
+          <h1 className="heading pb-3 text-p_red text-5xl">About us</h1>
           <div>
-            <h2 className="sub-heading">
-              At CreativeWorks, we believe in turning bold ideas into reality.
-              Founded by a passionate team of developers and designers, our
-              mission is to craft beautiful, functional, and impactful digital
-              experiences that leave a lasting impression.
+            <h2 className="sub-heading text-2xl py-2">
+              Welcome to{" "}
+              <span className="font-[700px] text-[20px] text-p_yello outlin py-0.5 px-1 rounded text-white bg-p_yellow rotate-6">
+                Rising Stars
+              </span>{" "}
+              – Where Little Minds Shine Bright!
             </h2>
+            <h3 className="para font-[500] text-[19px] pb-8">
+              To foster a <span className="text-p_red">love for learning,</span>{" "}
+              spark imagination, and help every child become kind,{" "}
+              <span className="text-p_blue">confident </span>
+              and <span className="text-p_green">capable.</span>
+            </h3>
+            <ul className="para text-[18px] space-y-3">
+              <li>
+                📚 A well-rounded curriculum blending academics, arts, and play
+              </li>
+              <li>
+                🧠 Focus on social-emotional development and confidence-building{" "}
+              </li>
+              <li>
+                🏃 Engaging indoor and outdoor activities for physical growth{" "}
+              </li>
+              <li>🧩 Small class sizes for personalized attention </li>
+            </ul>
           </div>
         </div>
 
-        <div className="relative bg-green-20 flex justify-center w-[520px]">
-          <img src={"/images/aboutimg.png"} className="w-[400px] relative -mt-10" />
-          <Image width={"700"} height={"400"} alt="svg" src={"/svg/blob1.svg"} className="w-full h-full -z-10 absolute top-4" />
+        <div className="relative bg-green-20 flex items-center justify-center">
+          <img src={"/images/aboutimg.png"} className="w-[500px] " />
+          <Image
+            width={"900"}
+            height={"400"}
+            alt="svg"
+            src={"/svg/blob1.svg"}
+            className="w-[550px] -z-10 absolute top-4"
+          />
         </div>
       </div>
+
+      {/* PROGRAMMS */}
+      <div className="my-28 relative text-p_red text-center">
+        <h1 className="heading pb-3 text-5xl">Programms</h1>
+        <Image
+          src={"/svg/Park Swing.svg"}
+          width={"80"}
+          height={"100"}
+          alt="paintbrush"
+          className="absolute left-10 bottom-12 opacity-65"
+        />
+        <Image
+          src={"/svg/Basketball.svg"}
+          width={"120"}
+          height={"100"}
+          alt="paintbrush"
+          className="absolute right-[450px] top-8 opacity-65"
+        />
+
+        <svg
+          className="absolute top-16 -rotate-3 w-[120rem] right-10 -left-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1440 320"
+        >
+          <path
+            fill="#FF6F61"
+            fillOpacity="0.3"
+            d="M0,192L30,165.3C60,139,120,85,180,80C240,75,300,117,360,160C420,203,480,245,540,261.3C600,277,660,267,720,240C780,213,840,171,900,170.7C960,171,1020,213,1080,208C1140,203,1200,149,1260,122.7C1320,96,1380,96,1410,96L1440,96L1440,320L1410,320C1380,320,1320,320,1260,320C1200,320,1140,320,1080,320C1020,320,960,320,900,320C840,320,780,320,720,320C660,320,600,320,540,320C480,320,420,320,360,320C300,320,240,320,180,320C120,320,60,320,30,320L0,320Z"
+          ></path>
+        </svg>
+
+        <div className="flex relative w-fit my-12 gap-10 mx-auto">
+          {[
+            {
+              id: 1,
+              title: "Pre-Nursery – A Joyful Start to Learning",
+              icon: "/svg/Kids.svg",
+              dec: "Introduce your little one to a world of wonder and discovery! Our Pre-Nursery program encourages play-based learning, language development, and social interaction in a safe, loving environment.",
+              age: "Age 2–3",
+              image: "/images/reading.png",
+            },
+            {
+              id: 2,
+              title: "Pre-Nursery – A Joyful Start to Learning",
+              icon: "/svg/Kids.svg",
+              dec: "Introduce your little one to a world of wonder and discovery! Our Pre-Nursery program encourages play-based learning, language development, and social interaction in a safe, loving environment.",
+              age: "Age 2–3",
+              image: "/images/reading.png",
+            },
+            {
+              id: 3,
+              title: "Pre-Nursery – A Joyful Start to Learning",
+              icon: "/svg/Kids.svg",
+              dec: "Introduce your little one to a world of wonder and discovery! Our Pre-Nursery program encourages play-based learning, language development, and social interaction in a safe, loving environment.",
+              age: "Age 2–3",
+              image: "/images/reading.png",
+            },
+            {
+              id: 4,
+              title: "Pre-Nursery – A Joyful Start to Learning",
+              icon: "/svg/Kids.svg",
+              dec: "Introduce your little one to a world of wonder and discovery! Our Pre-Nursery program encourages play-based learning, language development, and social interaction in a safe, loving environment.",
+              age: "Age 2–3",
+              image: "/images/reading.png",
+            },
+          ].map((card) => (
+            <ProgramCard key={card.id} card={card} id={card.id} />
+          ))}
+        </div>
+      </div>
+
+      {/* WHY */}
+
+      <div className="my-20 relative text-p_red text-center">
+        <h1 className="heading pb-3 text-5xl capitalize">why choose us</h1>
+        <h2 className="sub-heading text-lg ">
+          A nurturing start makes all the difference. Here’s why parents trust
+          us with their child’s early learning journey:
+        </h2>
+        <Image
+          src={"/svg/Maths Learning.svg"}
+          width={"300"}
+          height={"200"}
+          alt="paintbrush"
+          className="absolute left-10 top-12 opacity-65"
+        />
+
+        <div className="flex items-baseline relative w-fit my-12 gap-16 mx-auto">
+          {/* D1 */}
+          <div className="max-w-[250px]">
+            <div className="bg-p_yellow  outline-offset-8 rotate-6 outline-dashed outline-p_yellow p-2">
+              <Image
+                src={"/images/lib.png"}
+                width={200}
+                height={200}
+                alt="w1"
+                className="w-full"
+              />
+            </div>
+            <div className="my-4 ">
+              <h2 className="sub-heading text-lg text-dark_text">
+                Experienced & Caring Educators
+              </h2>
+              <p className="text-dark_text/80 para">
+                Our certified teachers specialize in early childhood education,
+                creating engaging, loving classrooms that feel like home.
+              </p>
+            </div>
+          </div>
+          {/* D2 */}
+          <div className="max-w-[250px]">
+            <div className="bg-p_green w-[220px] h-[220px] rounded-full overflow-hidden  outline-offset-8 rotate-[23deg] otline-dashed outline-p_green p-2">
+              <Image
+                src={"/images/lib.png"}
+                width={200}
+                height={200}
+                alt="w1"
+                className="w-full h-full rounded-full object-cover object-center"
+              />
+            </div>
+            <div className="my-5 ">
+              <h2 className="sub-heading text-lg text-dark_text">
+                Experienced & Caring Educators
+              </h2>
+              <p className="text-dark_text/80 para">
+                Our certified teachers specialize in early childhood education,
+                creating engaging, loving classrooms that feel like home.
+              </p>
+            </div>
+          </div>
+          {/* D3 */}
+          <div className="max-w-[250px]">
+            <div className="bg-p_blue h-[190px]  outline-offset-8 -rotate-6 outline-dashed outline-p_blue p-2">
+              <Image
+                src={"/images/lib.png"}
+                width={200}
+                height={200}
+                alt="w1"
+                className="w-full h-full object-cover object-center"
+              />
+            </div>
+            <div className="my-5 ">
+              <h2 className="sub-heading text-lg text-dark_text">
+                Experienced & Caring Educators
+              </h2>
+              <p className="text-dark_text/80 para">
+                Our certified teachers specialize in early childhood education,
+                creating engaging, loving classrooms that feel like home.
+              </p>
+            </div>
+          </div>
+          {/* D4 */}
+          <div className="max-w-[250px]">
+            <div
+              className="px-4 relative py-2  rotate-6 bg-p_yellow"
+              style={{
+                clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)",
+              }}
+            >
+              <div
+                className="w-full h-full"
+                style={{
+                  clipPath: "polygon(50% 0%, 0% 100%, 100% 100%)",
+                  overflow: "hidden",
+                }}
+              >
+                <Image
+                  src="/images/lib.png"
+                  width={200}
+                  height={200}
+                  alt="w1"
+                  className="w-full h-full object-cover object-center"
+                />
+              </div>
+            </div>
+
+            <div className="my-4 ">
+              <h2 className="sub-heading text-lg text-dark_text">
+                Experienced & Caring Educators
+              </h2>
+              <p className="text-dark_text/80 para">
+                Our certified teachers specialize in early childhood education,
+                creating engaging, loving classrooms that feel like home.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* CTA */}
+      <div className="relative flex flex-col items-center min-h-96 my-20 bg-green-5">
+        <Image
+          src={"/svg/3wave.svg"}
+          // src={"/svg/bottom-wave.svg"}
+          width={"600"}
+          height={"200"}
+          alt="paintbrush"
+          className="absolute w-full left-0 bottom-29 opacity-65"
+        />
+        <div className="absolute top-28 left-48  para text-xl text-white bg-p_yellow p-4 rounded-2xl -rotate-12">Call Us: +91-9876543210</div>
+        <div className="absolute top-28 right-48 heading text-6xl bg-white rounded-full p-6 w-[90px] flex justify-center items-center h-[90px] text-p_blue font-bold">+</div>
+        <Image
+          src={"/images/grp.png"}
+          width={"700"}
+          height={"200"}
+          alt="paintbrush"
+          className="relative z-10"
+        />
+
+        <div className="sub-heading text-xl max-w-3xl text-center relative">
+          <h2>
+            Give your little one the perfect start with fun-filled activities,
+            caring educators, and a nurturing environment that feels like home.
+          </h2>
+          <button className="px-6 py-3 my-3 bg-blue-400 hover:bg-blue-500 text-p_white font-semibold rounded-xl transition">
+            Apply for Admission
+          </button>
+        </div>
+      </div>
+
+      {/* FORM */}
+      <Form />
     </section>
   );
 }
---
+++ components/Form.jsx
@@ -0,0 +1,189 @@
+"use client";
+
+import React, { useState } from "react";
+import PersonIcon from "@mui/icons-material/Person";
+import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
+import EmailIcon from "@mui/icons-material/Email";
+import { IconButton, Snackbar } from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+
+const Form = () => {
+  const [query, setQuery] = useState({
+    name: "",
+    number: "",
+    email: "",
+    type: "",
+    inquiry: "",
+  });
+  const [querySent, setQuerySent] = useState(false);
+  const [queryduplicate, setQueryDuplicate] = useState(false);
+
+  // console.log(queryduplicate);
+
+  const handleClick = async () => {
+    try {
+      setQuerySent(false);
+
+      const response = await fetch("api/query/post", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ ...query }),
+      });
+      const data = await response.json();
+      alert(data.msg);
+      if (data.success) {
+        setQuerySent(true);
+      }
+    } catch (err) {
+      alert("some error occured, Try again later");
+      setQuerySent(false);
+      // setQueryDuplicate(true);
+    }
+  };
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setQuerySent(false);
+    setQueryDuplicate(false);
+  };
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
+  return (
+    <div className="bg-white/70 pb-10 h-full md:mx-72 px-6 mx-4 md:px-12 rounded-[30px] ">
+    
+      <form
+        className={`*:my-2 content-center pt-2 md:pt-6 md:w-full mx-auto  `}
+      >
+        <Snackbar
+          open={querySent}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message="Query sent successfully."
+          action={action}
+        />
+        <Snackbar
+          open={queryduplicate}
+          autoHideDuration={6000}
+          onClose={handleClose}
+          message="Query sent already ."
+          action={action}
+        />
+
+        <div className="md:flex items-center  *:my-2 *:md:my-0 *:w-full gap-4 *:border *:rounded *:flex *:items-center *:p-2.5 *:gap-2  ">
+          <div className="">
+            <PersonIcon />
+            <input
+              type="text"
+              id="name"
+              name="Name"
+              value={query.name}
+              onChange={(e) => setQuery({ ...query, name: e.target.value })}
+              placeholder="Full Name"
+              className="capitalize w-full outline-none bg-transparent px-2"
+            />
+          </div>
+          <div className="">
+            <LocalPhoneIcon />
+            <input
+              type="text"
+              id="number"
+              name="number"
+              value={query.number}
+              onChange={(e) => setQuery({ ...query, number: e.target.value })}
+              placeholder="Phone Number"
+              className="w-full outline-none bg-transparent px-2"
+            />
+          </div>
+        </div>
+        <div className="md:flex items-center *:my-2 *:md:my-0 *:w-full gap-4 *:border *:rounded *:flex *:items-center  *:gap-2  ">
+          <div className="p-2.5 ">
+            <EmailIcon />
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={query.email}
+              onChange={(e) => setQuery({ ...query, email: e.target.value })}
+              placeholder="Email"
+              className="w-full outline-none bg-transparent px-2"
+            />
+          </div>
+
+          <div className="">
+            <select
+              name="type"
+              defaultValue=""
+              className="w-full capitalize bg-transparent *:border-b *:border-text p-3 h-full focus:outline-none"
+              onChange={(e) =>
+                setQuery({ ...query, type: e.target.value.toUpperCase() })
+              }
+            >
+              <option value={query.type} disabled hidden className="">
+                Type of inquiry
+              </option>
+              <option value="Admission" className=" *:hover:font-semibold">
+                Admission
+              </option>
+              <option value="Fees" className=" *:hover:font-semibold">
+                Fees
+              </option>
+              <option value="Facilities" className=" *:hover:font-semibold">
+                Facilities
+              </option>
+              <option value="Programms" className=" *:hover:font-semibold">
+                Programms
+              </option>
+              <option value="other" className=" *:hover:font-semibold">
+                Other
+              </option>
+            </select>
+          </div>
+        </div>
+        <div className="border backdrop-blur-md rounded  flex items-center p-2 gap-2">
+          <textarea
+            rows={5}
+            id="inquiry"
+            name="inquiry"
+            value={query.inquiry}
+            onChange={(e) => {
+              console.log(query);
+              setQuery({ ...query, inquiry: e.target.value });
+            }}
+            placeholder="Tell us more about your needs or questions.."
+            className="w-full  outline-none bg-transparent px-2"
+          />
+        </div>
+        <div className="float-right flex items-center gap-6 md:py-3 py-2  ">
+          <button
+            className="bg-prim_black py-3 px-8 rounded-sm text-prim_white hover:bg-gray capitalize text-nowrap "
+            onClick={(e) => {
+              e.preventDefault();
+             handleClick()
+            }}
+          >
+            <p>Send</p>
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
